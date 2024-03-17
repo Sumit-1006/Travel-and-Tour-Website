12fb47cac2c0aa8ced8bf35890a9f20ee7e48349
Sumit-1006 <skkatiyar10603@gmail.com>
2024-03-17 23:39:09 +0530

    Added Modifications

--- src/Routess/Service.js
+++ src/Routess/Service.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import AboutImg from "../Assets/night.jpg";
+import AboutImg from "../Assets/4.jpg";
 import Footer from '../components/Footer';
 import Trip from "../components/Trip";
 function Service() {
